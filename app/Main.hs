dc3f2bb7c5f8814d636f2588bba92ce3e01a5347
spartanpotato <107315314+spartanpotato@users.noreply.github.com>
2024-10-31 19:45:40 -0300

    movimiento y colision basico

--- app/Main.hs
+++ app/Main.hs
@@ -6,16 +6,16 @@ import Graphics.Gloss.Interface.Pure.Game
 
 --Definicion de la ventana
 width, height, offset :: Int
-width = 300
-height = 300
+width = 640
+height = 480
 offset = 100
 
 window :: Display
-window = InWindow "Pong" (width, height) (offset, offset)
+window = InWindow "Canonwars" (width, height) (offset, offset)
 
 --Definicion color de fondo
 background :: Color
-background = black
+background = white
 
 --Definicion fps
 fps :: Int
@@ -25,101 +25,122 @@ main :: IO ()
 main = play window background fps initialState render handleKeys update
 
 --Definicion de tipo de dato que define el estado del juego, show solo esta para depurar
-data PongGame = Game
-  { ballLoc :: (Float, Float)  
-  , ballVel :: (Float, Float) 
-  , player1 :: Float           
-                               
-  , player2 :: Float           
+data World = Game
+  { playerVel :: Float,  
+    player1Loc :: (Float,Float),
+    player2Loc :: (Float,Float),
+    pillar :: (Float, Float, Float, Float),  
+    moveLeft :: Bool,  
+    moveRight :: Bool,
+    currentPlayer :: Int, -- Player1=1 Player2=2
+    direction :: Int -- izquierda=1 derecha=2         
   } deriving Show
 
 --Deficion estado inicial del juego
-initialState :: PongGame
+initialState :: World
 initialState = Game
-  { ballLoc = (-10, 30)
-  , ballVel = (90, -30)
-  , player1 = 40
-  , player2 = -80
+  { playerVel = 2,
+    player1Loc = (-(fromIntegral width / 4), -(fromIntegral height / 2) + 25),
+    player2Loc = (fromIntegral width/4, -(fromIntegral height / 2) + 25),
+    pillar = (0, -(fromIntegral height/2) + 70, 50, 100),
+    moveLeft = False,
+    moveRight = False,
+    currentPlayer = 1,
+    direction = 0
   }
 
 --Definicion de funcion para convertir estado del juego en una imagen
-render :: PongGame -> Picture  
-render game = pictures [ball, walls,
-            mkPaddle rose 120 $ player1 game,
-            mkPaddle orange (-120) $ player2 game]
+render :: World -> Picture  
+render game = pictures [player1, player2, mainFloor, mainPillar]
   where
-    ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid 10
-    ballColor = dark red
+    player1 = uncurry translate (player1Loc game) $ color player1Color $ rectangleSolid 10 10
+    player1Color = dark red
+    player2 = uncurry translate (player2Loc game) $ color player2Color $ rectangleSolid 10 10
+    player2Color = dark blue
 
-    wall :: Float -> Picture
-    wall offset =
-      translate 0 offset $
-        color wallColor $
-          rectangleSolid 270 10
+
+    makeFloor :: Float -> Float -> Float -> Float -> Picture
+    makeFloor offsetX offsetY floorWidth floorHeight = 
+        translate offsetX offsetY $ color wallColor $ rectangleSolid floorWidth floorHeight
 
     wallColor = greyN 0.5
-    walls = pictures [wall 150, wall (-150)]
+    mainFloor = makeFloor 0 (- (fromIntegral height/2) + 10) (fromIntegral width) 20
 
-    --  Make a paddle of a given border and vertical offset.
-    mkPaddle :: Color -> Float -> Float -> Picture
-    mkPaddle col x y = pictures
-      [ translate x y $ color col $ rectangleSolid 26 86
-      , translate x y $ color paddleColor $ rectangleSolid 20 80
-      ]
+    makePillar :: (Float, Float, Float, Float) -> Picture
+    makePillar (offsetX, offsetY, floorWidth, floorHeight) =
+        translate offsetX offsetY $ color pillarColor $ rectangleSolid floorWidth floorHeight
 
-    paddleColor = light (light blue)
+    pillarColor = greyN 0.5
+    mainPillar = makePillar (pillar game)
 
---Funcion que en base de segundos y el estado anterior del juego actualiza la posicion de la bola
-moveBall :: Float -> PongGame -> PongGame
-moveBall seconds game = game { ballLoc = (x', y') }
+
+--Funcion que en base de segundos y si se esta presionando una tecla mueve al jugador
+movePlayer :: Float -> World -> World
+movePlayer _ game
+    | moveLeft game = if currentPlayer game == 1
+        then game { player1Loc = (x - playerVel game, y) }
+        else game { player2Loc = (x - playerVel game, y) }
+    | moveRight game = if currentPlayer game == 1
+        then game { player1Loc = (x + playerVel game, y) }
+        else game { player2Loc = (x + playerVel game, y) }
+    | otherwise = game 
   where
-    -- Old locations and velocities.
-    (x, y) = ballLoc game
-    (vx, vy) = ballVel game
-
-    -- New locations.
-    x' = x + vx * seconds
-    y' = y + vy * seconds
+    (x, y) = player1Loc game
 
 --Funcion que 
-update ::  Float -> PongGame -> PongGame
-update seconds = wallBounce . moveBall seconds
+update ::  Float -> World -> World
+update seconds = wallBounce . movePlayer seconds
 
---Creacion de sinonimos
+
 type Radius = Float 
 type Position = (Float, Float)
 
---Funcion que verifica si posicion de la bola tiene colision con la de una pared
 wallCollision :: Position -> Radius -> Bool 
-wallCollision (_, y) radius = topCollision || bottomCollision
+wallCollision (x, _) radius = leftCollision || rightCollision || leftPillarCollision || rightPillarCollision
   where
-    topCollision    = y - radius <= -fromIntegral width / 2 
-    bottomCollision = y + radius >=  fromIntegral width / 2
+    leftCollision  = (x - radius <= - (fromIntegral width / 2))
+    rightCollision = (x + radius >= fromIntegral width / 2)
+    leftPillarCollision = (x - radius > -25 && x - radius < 25)
+    rightPillarCollision = (x + radius > -25 && x + radius < 25)
 
---Funcion que usa wallCollision para ver si la bola golpea una pared e invierte la velocidad correspondiente
-wallBounce :: PongGame -> PongGame
-wallBounce game = game { ballVel = (vx, vy') }
-  where
-    -- Radius. Use the same thing as in `render`.
-    radius = 10
 
-    -- The old velocities.
-    (vx, vy) = ballVel game
-
-    vy' = if wallCollision (ballLoc game) radius
-          then
-             -- Update the velocity.
-             -vy
-           else
-            -- Do nothing. Return the old velocity.
-            vy
+--Funcion que usa wallCollision para ver si el personaje esta en un limite y revierte su movimiento
+wallBounce :: World -> World
+wallBounce game
+    | currentPlayer game == 1 = game { player1Loc = (x', y) }
+    | currentPlayer game == 2 = game { player2Loc = (x', y) }
+    where
+        radius = 3 -- radio debe ser el radio original del personaje menos la velocidad
+        (x, y) = if currentPlayer game == 1 
+                 then player1Loc game 
+                 else player2Loc game
+        x' = if wallCollision (if currentPlayer game == 1 
+                                then player1Loc game 
+                                else player2Loc game) radius
+             then if direction game == 1  -- colision izquierda
+                  then x + playerVel game
+                  else x - playerVel game  -- colision derecha
+             else x
 
 
 -- Respond to key events.
-handleKeys :: Event -> PongGame -> PongGame
+handleKeys :: Event -> World -> World
 
--- For an 's' keypress, reset the ball to the center.
-handleKeys (EventKey (Char 's') _ _ _) game = game { ballLoc = (0, 0) }
+-- Player se mueve a la izquierda con a
+handleKeys (EventKey (Char 'a') Down _ _) game = 
+    game { moveLeft = True, moveRight = False, direction = 1}
+
+-- Player deja de moverse al dejar de presionar a
+handleKeys (EventKey (Char 'a') Up _ _) game = 
+    game { moveLeft = False}
+
+-- Player se mueve a la derecha con d
+handleKeys (EventKey (Char 'd') Down _ _) game = 
+    game { moveRight = True, moveLeft = False, direction = 2}
+
+-- Player deja de moverse al dejar de presionar d
+handleKeys (EventKey (Char 'd') Up _ _) game = 
+    game { moveRight = False}
 
 -- Do nothing for all other events.
 handleKeys _ game = game
