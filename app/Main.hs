8b99b9e52ec92876da01c385d698a49528905ad6
MaDummy <alvaro.burgosandrade@gmail.com>
2024-11-02 16:33:12 -0300

    Codigo modificado para ser escalable

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,7 @@ import Graphics.Gloss.Interface.Pure.Game
 import Combustible
 import Colores
 import System.Random
+
 --Definicion de la ventana
 width, height, offset :: Int
 width = 640
@@ -23,69 +24,116 @@ background = blue' --makeColor 0.8549 0.9961 1.0 1.0
 fps :: Int
 fps = 60
 
-
 main :: IO ()
 main = do
     gen' <- newStdGen
     let initialGame = initialState{gen = gen'}
     play window background fps initialGame render handleKeys update
 
+data Tank = Tank
+  { position :: (Float, Float)  -- Posición del tanque (x, y)
+  , angle    :: Float           -- Ángulo de dirección del tanque
+  , bodySize :: (Float, Float)  -- Tamaño del cuerpo (ancho, alto)
+  , cannonSize :: (Float, Float) -- Tamaño del cañón (ancho, largo)
+  , colorBody :: Color          -- Color del cuerpo del tanque
+  , colorCannon :: Color        -- Color del cañón del tanque
+  , tankVel :: Float            -- Velocidad del tanque
+  , moveLeft :: Bool           -- Movimiento hacia la izquierda
+  , moveRight :: Bool           -- Movimiento hacia la derecha
+  , direction :: Int            -- Dirección (0 = izq, 1 = der)
+  , fuelBar :: (Float, Float, Float, Float)
+  , amountFuel :: Float
+  , currentFuelBar :: (Float, Float, Float, Float)
+  , offsetBar :: Float
+  , barWidth :: Float
+  , shotUsage :: Float
+  , moveUsage :: Float
+  , cannonUsage :: Float
+  , percentage :: String
+  } deriving (Show)
 --Definicion de tipo de dato que define el estado del juego, show solo esta para depurar
 data World = Game
-  { playerVel :: Float,  
-    player1Loc :: (Float,Float),
-    player2Loc :: (Float,Float),
-    pillar :: (Float, Float, Float, Float),  
-    moveLeft :: Bool,  
-    moveRight :: Bool,
+  { player1 :: Tank,
+    player2 :: Tank,
+    pillar :: (Float, Float, Float, Float),
     currentPlayer :: Int, -- Player1=1 Player2=2
-    direction :: Int, -- izquierda=1 derecha=2
-    fuelBar :: (Float, Float, Float, Float), -- dimensiones de la barra del combustible (como el exterior, no la barra que vemos gastarse)
-    amountFuel :: Float, -- Cantidad de combustible disponible
-    currentFuelBar :: (Float, Float, Float, Float), -- dimensiones de la barra del combustible actualmente disponible
-    barWidth :: Float, -- ancho original de la barra de combustible disponible
-    shotUsage :: Float, -- cuanto se usará de combustible al disparar (20)
-    moveUsage :: Float, -- cuanto se usará de combustible al moverse el personaje (10)
-    cannonUsage :: Float, -- cuanto se usará de combustible al mover el cañon (5)
-    offsetBar :: Float, -- util actualizar la posición de la barra de combustible
-    percentage :: String,
-    gen :: StdGen -- Para el calculo de probabilidades y aleatoridad
+    gen :: StdGen
   } deriving Show
 
 
---Deficion estado inicial del juego
+-- Se define el estado inicial del juego
 initialState :: World
-initialState = Game
-  { playerVel = 2,
-    player1Loc = (-(fromIntegral width / 4), -(fromIntegral height / 2) + 25),
-    player2Loc = (fromIntegral width/4, -(fromIntegral height / 2) + 25),
+initialState = Game {
+    player1 = Tank {
+        position = (-(fromIntegral width / 4), -(fromIntegral height / 2) + 25),
+        angle = 45,
+        bodySize = (60, 20),
+        cannonSize = (6, 25),
+        colorBody = dark $ dark green,
+        colorCannon = greyN 0.3,
+        tankVel = 2,
+        moveLeft = False,
+        moveRight = False,
+        direction = 0,
+        fuelBar = (-200, 200, 200, 50),
+        amountFuel = 100,
+        currentFuelBar = (-200, 200, 180, 31),
+        barWidth = 180,
+        shotUsage = 20, 
+        moveUsage = 10,
+        cannonUsage = 5,
+        offsetBar = -200,
+        percentage = "100%"
+    },
+    player2 = Tank {
+        position = (fromIntegral width/4, -(fromIntegral height / 2) + 25),
+        angle = 45,
+        bodySize = (60, 20),
+        cannonSize = (6, 25),
+        colorBody = dark $ dark green,
+        colorCannon = greyN 0.3,
+        tankVel = 2,
+        moveLeft = False,
+        moveRight = False,
+        direction = 0,
+        fuelBar = (-200, 200, 200, 50),
+        amountFuel = 100,
+        currentFuelBar = (-200, 200, 180, 31),
+        barWidth = 180,
+        shotUsage = 20, 
+        moveUsage = 10,
+        cannonUsage = 5,
+        offsetBar = -200,
+        percentage = "100%"
+    },
     pillar = (0, -(fromIntegral height/2) + 70, 50, 100),
-    moveLeft = False,
-    moveRight = False,
     currentPlayer = 1,
-    direction = 0,
-    fuelBar = (-200, 200, 200, 50), 
-    amountFuel = 100, -- inicia en el maximo (antes: fuel)
-    currentFuelBar = (-200, 200, 180, 31), 
-    barWidth = 180,
-    shotUsage = 20, 
-    moveUsage = 10,
-    cannonUsage = 5,
-    offsetBar = -200,
-    percentage = "100%",
     gen = mkStdGen 0
-  }
+}
     
+currentTank :: World -> Tank
+currentTank game
+  | currentPlayer game == 1 = player1 game
+  | otherwise               = player2 game
+
+setCurrentTank :: Tank -> World -> World
+setCurrentTank tank game
+  | currentPlayer game == 1 = game { player1 = tank }
+  | otherwise               = game { player2 = tank }
+
+updatePosition :: Float -> Tank -> Tank
+updatePosition dx tank = tank { position = (x + dx, y) }
+  where
+    (x, y) = position tank
 
 --Definicion de funcion para convertir estado del juego en una imagen
 render :: World -> Picture  
-render game = pictures [player1, player2, mainFloor, mainPillar, mainFuelBar, mainTotalFuel, mainPercent]
+render game = pictures [renderTank (player1 game), renderTank (player2 game), mainFloor, mainPillar, mainFuelBar, mainTotalFuel, mainPercent]
   where
-    player1 = uncurry translate (player1Loc game) $ color player1Color $ rectangleSolid 10 10
-    player1Color = yellow' -- dark red
-    player2 = uncurry translate (player2Loc game) $ color player2Color $ rectangleSolid 10 10
-    player2Color = orange' -- dark blue
-
+    -- Funcion para renderizar un tanque
+    renderTank :: Tank -> Picture
+    renderTank tank = pictures [uncurry translate (position tank) $ color (colorBody tank) $ rectangleSolid (fst $ bodySize tank) (snd $ bodySize tank),
+                                uncurry translate (position tank) $ color (colorCannon tank) $ rectangleSolid (fst $ cannonSize tank) (snd $ cannonSize tank)]
 
     makeFloor :: Float -> Float -> Float -> Float -> Picture
     makeFloor offsetX offsetY floorWidth floorHeight = 
@@ -101,24 +149,19 @@ render game = pictures [player1, player2, mainFloor, mainPillar, mainFuelBar, ma
     pillarColor = darkBlue -- greyN 0.5
     mainPillar = makePillar (pillar game)
 
-    mainFuelBar = makeFuelBar (fuelBar game)
-    mainTotalFuel = totalFuel (currentFuelBar game)
-    mainPercent = showFuel (-290, 190, 0.2 , 0.2, amountFuel game)
-
+    mainFuelBar = makeFuelBar (fuelBar (currentTank game))
+    mainTotalFuel = totalFuel (currentFuelBar (currentTank game))
+    mainPercent = showFuel (-290, 190, 0.2 , 0.2, amountFuel (currentTank game))
 
 
 --Funcion que en base de segundos y si se esta presionando una tecla mueve al jugador
 movePlayer :: Float -> World -> World
 movePlayer _ game
-    | moveLeft game = if currentPlayer game == 1
-        then game { player1Loc = (x - playerVel game, y) }
-        else game { player2Loc = (x - playerVel game, y) }
-    | moveRight game = if currentPlayer game == 1
-        then game { player1Loc = (x + playerVel game, y) }
-        else game { player2Loc = (x + playerVel game, y) }
-    | otherwise = game 
-  where
-    (x, y) = player1Loc game
+    | moveLeft tank = setCurrentTank (updatePosition (-tankVel tank) tank) game
+    | moveRight tank = setCurrentTank (updatePosition (tankVel tank) tank) game
+    | otherwise = game
+    where
+        tank = currentTank game
 
 --Funcion que 
 update ::  Float -> World -> World
@@ -139,21 +182,18 @@ wallCollision (x, _) radius = leftCollision || rightCollision || leftPillarColli
 
 --Funcion que usa wallCollision para ver si el personaje esta en un limite y revierte su movimiento
 wallBounce :: World -> World
-wallBounce game
-    | currentPlayer game == 1 = game { player1Loc = (x', y) }
-    | currentPlayer game == 2 = game { player2Loc = (x', y) }
-    where
+wallBounce game = 
+    let tank = if currentPlayer game == 1 then player1 game else player2 game
         radius = 3 -- radio debe ser el radio original del personaje menos la velocidad
-        (x, y) = if currentPlayer game == 1 
-                 then player1Loc game 
-                 else player2Loc game
-        x' = if wallCollision (if currentPlayer game == 1 
-                                then player1Loc game 
-                                else player2Loc game) radius
-             then if direction game == 1  -- colision izquierda
-                  then x + playerVel game
-                  else x - playerVel game  -- colision derecha
+        (x, y) = position tank
+        x' = if wallCollision (position tank) radius
+             then if direction tank == 1  -- colision izquierda
+                  then x + tankVel tank
+                  else x - tankVel tank  -- colision derecha
              else x
+    in if currentPlayer game == 1
+        then game {player1 = tank {position = (x', y)}}
+        else game {player2 = tank {position = (x', y)}}
 
 
 -- Respond to key events.
@@ -163,44 +203,61 @@ handleKeys :: Event -> World -> World
 
 -- Player se mueve a la izquierda con a
 handleKeys (EventKey (Char 'a') Down _ _) game = 
-    let f = amountFuel game
-        u = moveUsage game
-        o = offsetBar game
-        b = barWidth game
-     in if ((f - u) > 0 ) 
-      then game {moveLeft = True, moveRight = False, direction = 1, amountFuel = f - u, 
-      currentFuelBar = (o - (u-1), 200, ((f - u) / 100) * b, 31), offsetBar = o - (u-1)} 
+    let tank = currentTank game
+        fuel = amountFuel tank
+        usage = moveUsage tank
+        offset = offsetBar tank
+        barW = barWidth tank
+        newFuel = fuel - usage
+     in if newFuel > 0
+      then updateGame tank {moveLeft = True, moveRight = False, direction = 1, amountFuel = newFuel, 
+                            currentFuelBar = (offset - (usage-1), 200, (newFuel / 100) * barW, 31),
+                            offsetBar = offset - (usage-1)}
       else game
+    where
+        updateGame t = if currentPlayer game == 1 then game {player1 = t} else game {player2 = t}
 
 -- Player deja de moverse al dejar de presionar a
-handleKeys (EventKey (Char 'a') Up _ _) game = 
-    game { moveLeft = False}
+handleKeys (EventKey (Char 'a') Up _ _) game = updateGame (currentTank game) {moveLeft = False}
+    where
+        updateGame t = if currentPlayer game == 1 then game {player1 = t} else game {player2 = t}
 
 -- Player se mueve a la derecha con d
 handleKeys (EventKey (Char 'd') Down _ _) game = 
-    let f = amountFuel game
-        u = moveUsage game
-        o = offsetBar game
-        b = barWidth game
-     in if ((f - u) > 0 ) 
-      then game {moveRight = True, moveLeft = False, direction = 2, amountFuel = f - u, 
-      currentFuelBar = (o - (u-1), 200, ((f - u) / 100) * b, 31), offsetBar = o - (u-1)} 
+    let tank = currentTank game
+        fuel = amountFuel tank
+        usage = moveUsage tank
+        offset = offsetBar tank
+        barW = barWidth tank
+        newFuel = fuel - usage
+    in if newFuel > 0
+      then updateGame tank {moveRight = True, moveLeft = False, direction = 2, amountFuel = newFuel, 
+                            currentFuelBar = (offset - (usage-1), 200, (newFuel / 100) * barW, 31),
+                            offsetBar = offset - (usage-1)} 
       else game
+    where
+        updateGame t = if currentPlayer game == 1 then game {player1 = t} else game {player2 = t}
 
 -- Player deja de moverse al dejar de presionar d
-handleKeys (EventKey (Char 'd') Up _ _) game = 
-    game { moveRight = False}
+handleKeys (EventKey (Char 'd') Up _ _) game = updateGame (currentTank game) {moveLeft = False}
+    where
+        updateGame t = if currentPlayer game == 1 then game {player1 = t} else game {player2 = t}
 
 
 -- Combustible se consume al hacer uso de el 
 handleKeys (EventKey (Char 'e') Up _ _) game = 
-    let f = amountFuel game
-        u = shotUsage game
-        o = offsetBar game
-        b = barWidth game
-     in if ((f - u) >= 0 ) 
-      then game {amountFuel = f - u, currentFuelBar = (o - (u - 2), 200, ((f - u) / 100) * b, 31), offsetBar = o - (u - 2)} 
+    let tank = currentTank game
+        fuel = amountFuel tank
+        usage = shotUsage tank
+        offset = offsetBar tank
+        barW = barWidth tank
+        newFuel = fuel - usage
+    in if newFuel > 0
+      then updateGame tank {amountFuel = newFuel, currentFuelBar = (offset - (usage - 2), 200, (newFuel / 100) * barW, 31),
+                            offsetBar = offset - (usage - 2)} 
       else game 
+    where
+        updateGame t = if currentPlayer game == 1 then game {player1 = t} else game {player2 = t}
 
 -- Do nothing for all other events.
 handleKeys _ game = game
