09c8e5c3cce01382ac220bf4da761282ec8f003b
NigelMoon <107816792+NigelMoon@users.noreply.github.com>
2024-11-10 21:57:19 -0300

    Comentado toda Bala.hs

--- app/Bala.hs
+++ app/Bala.hs
@@ -26,7 +26,7 @@ width = 640
 height = 480
 offset = 100
 
-
+-- Bala default para definir inicialmente
 defaultBullet :: Bullet
 defaultBullet = Bullet
     { bPosition = (0,0)
@@ -34,7 +34,10 @@ defaultBullet = Bullet
     , bDamage = 0
     }
 
--- definir la bala
+-- Funcion que crea la bala
+-- Toma la posicion del jugador, el largo y el angulo del cañon,
+-- la velocidad, el id del jugador actual y el generador, devolviendo el
+-- generador actualizado y una bala
 createBullet :: (Float, Float) -> Float -> Float -> Float -> Int -> StdGen -> (Bullet,StdGen)
 createBullet (xPlayer, yPlayer) length anglePlayer velPlayer idPlayer gen = 
     let newAngle = if idPlayer == 1 then pi/2 - anglePlayer else (-anglePlayer + pi/2)
@@ -45,6 +48,9 @@ createBullet (xPlayer, yPlayer) length anglePlayer velPlayer idPlayer gen =
 
 -- posicion de la bala para que salga delante del cañon
 
+-- Funcion que calcula la posición de donde comienza la bala
+-- Toma la posicion, el angulo y la mitad del largo del cañon como argumento
+-- devolviendo la posicion de donde saldra
 calcPos :: (Float, Float) -> Float -> Float -> (Float, Float)
 calcPos (xPos, yPos) angle radio =
     let x = xPos + radio * cos(angle) + bulletRadius
@@ -53,14 +59,17 @@ calcPos (xPos, yPos) angle radio =
     in result
 
 
--- calculo del vector de velocidad de la bala
+-- Funcion que calcula la velocidad con la que saldra la bala
+-- Toma como argumento la velocidad inicial, el angulo y el generador
+-- devuelve la velocidad en forma de vector y el generador
 calcVelocity:: Float -> Float -> StdGen -> ((Float,Float),StdGen)
 calcVelocity vel angle gen =
     let (plusAngle,gen') = randomR(((-5)*(pi/180)),(5*(pi/180))) gen :: (Float, StdGen)
         finalAngle = angle + plusAngle
     in ((vel*cos(finalAngle),vel*sin(finalAngle)), gen')
 
--- calculo de daño que realizara la bala
+-- Funcion que devuelve el daño realizado por la bala
+-- toma el generador y devuelve el daño realizado y el generador
 calcDmg:: StdGen -> (Int,StdGen)
 calcDmg gen =
     let (initialDmg,gen') = randomR (1,3) gen :: (Int,StdGen)
@@ -71,16 +80,22 @@ calcDmg gen =
 -- Fin definiciones para Bullet especifica
 
 -- Función que dibuja la bala
+-- toma como argumento la bala y el estado de la bala
+-- devuelve la imagen a dibujar
 drawBullet :: Bullet -> Bool -> Picture
 drawBullet (Bullet (x, y) _ _) value = if value then translate x y $ color red $ circleSolid (bulletRadius*2) else blank
 
-
+-- Funcion que itera el movimiento simple
+-- toma el estado de la bala, los segundos y la bala,
+-- devolviendo la bala actualizada en posicion y su estado
 moveBullet :: Bool -> Float -> Bullet -> (Bullet,Bool)
 moveBullet shoot seconds bullet
     | shoot = (updateBullet seconds bullet)
     | otherwise = (bullet,False)
 
-
+-- Funcion que actualiza la posicion de la bala
+-- toma como argumentos la bala y los segundos
+-- que retorna la nueva posicion de la bala y su estado
 updateBullet :: Float -> Bullet -> (Bullet,Bool)
 updateBullet seconds bullet =
     let (x, y) = bPosition bullet
@@ -107,7 +122,9 @@ updateBullet seconds bullet =
         shot = newVelocity /= (0,0)
     in (bullet { bPosition = newPosition, bVelocity = newVelocity },shot)
 
-
+-- Funcion que verifica la colision con un tanque
+-- toma como argumentos la bala y el tanque oponente
+-- devolviendo verdadero si impacta y falso si no
 collitionBullet :: Bullet -> Tank -> Bool
 collitionBullet bullet tank =
     let (posBullX, posBullY) = bPosition bullet
